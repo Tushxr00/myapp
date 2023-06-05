acd6e85451f6c98e6b920a32e743ffc1e143ecf5
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-06-05 10:57:20 +0530

    REFACTOR: checking commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ void main() {  runApp( MaterialApp(
               fontWeight: FontWeight.bold,
               letterSpacing: 2.0,
               color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
-
-
+              fontFamily: 'IndieFlower'xw
           ),),),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
