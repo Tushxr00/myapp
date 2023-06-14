391ddc50b0d59adac61e393dd6c0149b72545748
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-06-14 17:35:55 +0530

    FEAT: added row widget in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,26 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blueGrey[600],
       ),
-      body:Padding(
-        padding: EdgeInsets.all(90),
-        child: Text('hellu'),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('hello world'),
+          TextButton(
+            onPressed: () {print('chal nah');},
+            style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20, color: Colors.white),
+                backgroundColor: Colors.amber),
+            child:
+                const Text('click me', style: TextStyle(color: Colors.white)),
+          ),
+            Container(
+              color: Colors.cyan,
+              padding: const EdgeInsets.all(30),
+              child: const Text(' inside a container'),
+            ),
+        ],
       ),
-
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print('you clicked me');
