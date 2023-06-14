bad1b89394a5a2d4a40afd0f7d80a59935de675e
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-06-14 15:35:54 +0530

    FEAT: added icon button

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {  runApp(const MaterialApp(
-    home:  Home()
-  ));
-
+void main() {
+  runApp(const MaterialApp(home: Home()));
 }
 
 class Home extends StatelessWidget {
@@ -12,37 +10,39 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        title: const  Text('My first App',),
+      appBar: AppBar(
+        title: const Text(
+          'My first App',
+        ),
         centerTitle: true,
         backgroundColor: Colors.blueGrey[600],
       ),
-      body:  Center(
-        child:  ElevatedButton.icon(
-            onPressed: (){
-              print('mailed you');
-            },
-            icon:const Icon(
-                Icons.mail,
-              color: Colors.black,
-            ),
-            label: const Text(
-              'Mail me',
-              style:TextStyle(
-               color: Colors.black,
-                  fontSize: 20
-              )
-              ,
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.amber,
-
-            ),
-
-        )
+      // body: Center(
+      //     child: ElevatedButton.icon(
+      //   onPressed: () {
+      //     print('mailed you');
+      //   },
+      //   icon: const Icon(
+      //     Icons.mail,
+      //     color: Colors.black,
+      //   ),
+      //   label: const Text(
+      //     'Mail me',
+      //     style: TextStyle(color: Colors.black, fontSize: 20),
+      //   ),
+      //   style: ElevatedButton.styleFrom(
+      //     backgroundColor: Colors.amber,
+      //   ),
+      // )),
+      body: Center(
+        child: IconButton(
+          onPressed: (){print('haha');},
+          icon: Icon(Icons.bluetooth_connected_sharp),
+          color: Colors.amber,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           print('you clicked me');
         },
         backgroundColor: Colors.blueGrey[600],
